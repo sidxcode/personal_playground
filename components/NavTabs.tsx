61d82b17701ae8ff18de57da7dec84748ceebffe
Siddharth <sid@Siddharths-MacBook-Air.local>
2026-04-07 22:15:39 +0530

    font size change

--- components/NavTabs.tsx
+++ components/NavTabs.tsx
@@ -14,13 +14,13 @@ export default function NavTabs() {
         <div className="w-full text-center md:w-auto md:text-left">
           <Link
             href="/"
-            className="font-[var(--font-geist-sans)] text-base font-medium tracking-tight hover:opacity-70"
+            className="font-[var(--font-geist-sans)] text-[18px] font-medium tracking-tight hover:opacity-70"
           >
             SIDDHARTH
           </Link>
         </div>
         <div className="w-full md:w-auto">
-          <ul className="flex flex-row items-center justify-between gap-2 px-5 text-sm font-medium md:justify-end">
+          <ul className="flex flex-row items-center justify-between gap-2 px-5 text-[18px] font-medium md:justify-end">
             {tabs.map((tab) => (
               <li key={tab.href}>
                 <Link
